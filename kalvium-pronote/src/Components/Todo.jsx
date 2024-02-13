cde3648b75e646056a7f9cb7be8673ccb6d7ea56
Pavithra8805 <pavithra8805@gmail.com>
2024-02-13 10:52:04 +0000

    Todo

--- kalvium-pronote/src/Components/Todo.jsx
+++ kalvium-pronote/src/Components/Todo.jsx
@@ -1,92 +1,75 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 
-
-class Todo extends Component {
+export class Todo extends Component {
     constructor() {
-        super();
+        super()
         this.state = {
-            inputText: "",
-            todos: [],
-        };
+            inputText: ' ',
+            todos: []
+        }
     }
-
     handleChange = (event) => {
         let userInput = event.target.value;
         this.setState({ inputText: userInput });
-    };
-
+    }
     handleClick = () => {
         this.setState({
             ...this.state,
             todos: [...this.state.todos, this.state.inputText],
-            inputText: "",
-        })
-    };
-
-    handleUpdates = (index) => {
-        let updateInput = prompt("Update todo..!!");
-
+            inputText: ' ',
+        });
+    }
+    handleUpdate = (index) => {
+        let updateInput = prompt("Update todo");
         let filterTodos = this.state.todos.map((el, i) => {
             if (i == index) {
                 return updateInput;
             }
             return el;
-        })
+        });
+        this.setState({ ...this.state, todos: filterTodos });
+    }
+    handleDelete = (index) => {
+        let todos = this.state.todos
+        todos.splice(index, 1)
+        this.setState({ ...this.setState, todos })
 
-        this.setState({ ...this.state,todos: filterTodos });
     }
 
-    handleDelete=(index) => {
-        let todos = this.state.todos;
-        todos.splice(index,1);
-        this.setState({ ...this.state,todos});
+    handleClick = () => {
+        const trimmedText = this.state.inputText.trim();
+        if (trimmedText !== '') {
+            this.setState({
+                todos: [...this.state.todos, trimmedText],
+                inputText: ''
+            });
+        } else {
+            alert('Please enter a non-empty todo.');
+        }
     }
-    handleConsole =()=> {
-        console.log(this.state);
-    }
-
 
     render() {
         return (
-            <>
-                <div>
-                    <h1>Todo Application</h1>
+            <div>
+                <h1>Todo Application</h1>
+                <div className='main-container'>
+                    <input type="text" placeholder="Whats on your mind?" onChange={this.handleChange} value={this.state.inputText} />
+                    <button onClick={this.handleClick}>Add Todo</button>
+                    {
+                        this.state.todos.map((el, i) => {
+                            return (
+                                <div key={i}>
+                                    <p>{el}</p>
+                                    <button onClick={() => this.handleUpdate(i)}>Update</button>
+                                    <button onClick={() => this.handleDelete(i)}>Delete</button>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-                <div className="main-container">
-                    <input
-                        type="text"
-                        placeholder="What's on your mind ?"
-                        onChange={this.handleChange}
-                        value={this.state.inputText}
-                    />
-                    <button onClick={this.handleClick}>
-                        Add Todo
-                    </button>
-                    <button onClick={this.handleConsole}>
-                        Console
-                    </button>
-                    
-
-                    {this.state.todos.map((el, i) => {
-                        return (
-                            <div key={i}>
-                                <p>{el}</p>
-                                <button onClick={() => this.handleUpdates(i)}>
-                                    Update
-                                </button>
-                                <button onClick={() => this.handleDelete(i)}>
-                                    Delete
-                                </button>
-                            </div>
-                        )
-                    })}
-                </div>
-            </>
+            </div>
         );
     }
 }
 
 export default Todo;
-
-
-
